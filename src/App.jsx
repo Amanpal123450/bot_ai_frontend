1c06d9bf4b568df053c14f6365b5740fb770ec45
Aman Pal <amanpal6000@gmail.com>
2026-09-23 18:53:43 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 
 import React from "react";
 import Chatbot from "./components/Chatbot";
-import Character from "./components/Character";
 
 function App() {
   return (
-    // <div style={{ fontFamily: "sans-serif" }}>
-    //   <h1 style={{ padding: 20 }}>Kikoo Website (example page)</h1>
+    <div style={{ fontFamily: "sans-serif" }}>
+      <h1 style={{ padding: 20 }}>Kikoo Website (example page)</h1>
 
-    //   <div style={{ position: "fixed", bottom: 20, right: 20 }}>
-    //     <Chatbot />
-    //   </div>
-    // </div>
-    <Character />
+      <div style={{ position: "fixed", bottom: 20, right: 20 }}>
+        <Chatbot />
+      </div>
+    </div>
   );
 }
 
